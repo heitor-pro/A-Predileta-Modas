674b41a31cb1cf51666889aa9bd57476fa1544f3
Heitor <heitorgratuliano@gmail.com>
2026-07-31 13:59:55 -0300

    update supabase types

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,6 +4,7 @@ import type { Database } from '@/types/database';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
+
 if (!supabaseUrl || !supabaseAnonKey) {
   // eslint-disable-next-line no-console
   console.error(
